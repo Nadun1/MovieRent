6d8c59205f70506819afb2b80c1931232ebe6fd1
Nadun1 <nadunsinhabahu@gmail.com>
2017-06-09 10:40:11 +0530

    Add project files.

---
+++ MovieRent/Models/MyDbSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieRent.Models
+{
+    public class MyDbSet : DbSet
+    {
+        public MyDbSet() : base("MovieRent")
+        {
+
+        }
+    }
+}
